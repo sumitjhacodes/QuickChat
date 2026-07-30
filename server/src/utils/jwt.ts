1f9c60507b160fd81e6ba3083ca91e7bd63cf2f9
devlopersumit <sumitsj2002@gmail.com>
2026-07-30 20:43:00 +0530

    setup jwt util function to reuse it

---
+++ server/src/utils/jwt.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const getJWTSecret = (): string => {
+  const secret = process.env.JWT_SECRET;
+  if (!secret) {
+    throw new Error("JWT_SECRET is not defined");
+  }
+  return secret;
+};
+
+export const generateJWTToken = (payload: string | object | Buffer) => {
+  return jwt.sign(payload, getJWTSecret(), {
+    expiresIn: 15,
+  });
+};
+
+export const verifyJWTToken = (token: string) => {
+  return jwt.verify(token, getJWTSecret());
+};
