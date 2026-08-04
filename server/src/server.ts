3697639c7da9fb2089a33c30a81ca77d8ccf0b23
devlopersumit <sumitsj2002@gmail.com>
2026-08-04 19:06:32 +0530

    chore: bootstrap server entrypoint with socket integration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { createServer } from "node:http";
-import { Server } from "socket.io";
-
 import app from "./app.js";
+import { attachSocketServer } from "./socket/socketServer.js";
 
 const server = createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: process.env.CORS_ORIGIN ?? "*",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("Socket connected:", socket.id);
-});
+attachSocketServer(server);
 
 const port = Number(process.env.PORT ?? 8000);
 
